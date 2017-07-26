5663fe1938e9fe48314006a9de772fad9b7af329
amhursh <amhursh@gmail.com>
2017-07-26 09:14:39 -0600

    Build out activity class with add_participant and total_cost methods and pass tests

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,21 @@
 class Activity
+
+  attr_reader :name,
+              :participants
+
+  def initialize(name)
+    @name         = name
+    @participants = {}
+  end
+
+  def add_participant(name, cost)
+    participants[name] = cost
+  end
+
+  def total_cost
+    participants.values.inject(0) do |sum, cost|
+      sum + cost
+    end
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,50 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
+  def test_activity_exists
+    activity = Activity.new("Brunch")
+
+    assert_instance_of Activity, activity
+  end
+
+  def test_acitivity_has_name
+    act = Activity.new("Brunch")
+
+    assert_equal "Brunch", act.name
+  end
+
+  def test_activity_has_no_participants_at_start
+    act = Activity.new("Brunch")
+
+    assert_equal ({}), act.participants
+  end
+
+  def test_activity_can_add_participants
+    act = Activity.new("Brunch")
+    act.add_participant("Jim", 20)
+
+    assert_equal 1, act.participants.count
+  end
+
+  def test_activity_can_check_total_cost
+    act = Activity.new("Brunch")
+    act.add_participant("Jim", 20)
+
+    assert_equal 20, act.total_cost
+  end
+
+  def test_activity_can_have_multiple_participants
+    act = Activity.new("Brunch")
+    act.add_participant("Jim", 20)
+
+    assert_equal 1, act.participants.count
+    assert_equal 20, act.total_cost
+
+    act.add_participant("Joe", 40)
+
+    assert_equal 2, act.participants.count
+    assert_equal 60, act.total_cost
+  end
+
 end
